c42b115bf5439dfb1506676c3dac90e8eb452b6d
Monatg55 <temeter@yahoo.com>
2016-05-23 11:10:27 +0200

    letzten Aufgäble

--- source/aufgabe7.cpp
+++ source/aufgabe7.cpp
@@ -5,6 +5,7 @@
 #include "vec2.hpp"
 #include "mat2.hpp"
 #include <vector>
+#include <algorithm>
 
 template <typename T>
 void swaps(T & a, T & b){
@@ -12,7 +13,36 @@ void swaps(T & a, T & b){
 	a = b;
 	b = c;
 }
+//3.11
+/*template <typename container, typename praedikat>
+container filter(container const& a, praedikat const& b){
+	container a1;
+	for (unsigned i = 0; i < a.size(); ++i) {
+		if(b(container[i])) {
+			a1.push_back(container[i]);
+		}
+	}
+	return a1;
+}*/
 
+template <typename Container, typename Praedikat>
+Container filter(Container const& a, Praedikat const& b){
+	Container a1;
+	for (unsigned i = 0; i < a.size(); ++i) {
+		if(b(a[i])) {
+			a1.push_back(a[i]);
+		}
+	}
+	return a1;
+}
+
+bool is_even(int a){
+	if (a % 2 == 0)
+		return true;
+	return false;
+}
+
+//3.3
 TEST_CASE("describe_factorial","[aufgabe3]"){
 	std::vector<Circle>v(5);
 	Circle a(Vec2(0.0f, 0.0f), 1.0f);
@@ -28,31 +58,46 @@ TEST_CASE("describe_factorial","[aufgabe3]"){
 	std::sort(std::begin(v), std::end(v));
 	REQUIRE(std::is_sorted(v.begin(), v.end()));
 }
-
+//3.8
 TEST_CASE("describe_swap","[aufgabe8]"){
 	int a = 1;
 	int b = 2;
 	swaps(a, b);
 	REQUIRE(b == 1);
 }
-
+//3.9
 TEST_CASE("describe_sort", "[aufgabe9]"){
-	std::vector<Circle>v(5);
-	Circle a(Vec2(0.0f, 0.0f), 1.0f);
-	Circle b(Vec2(0.0f, 0.0f), 6.0f);
-	Circle c(Vec2(0.0f, 0.0f), 3.0f);
-	Circle d(Vec2(0.0f, 0.0f), 4.0f);
-	Circle e(Vec2(0.0f, 0.0f), 5.0f);
+	std::vector<Circle> v;
+	Circle a{ Vec2(0.0f, 0.0f), 1.0f };
+	Circle b{ Vec2(0.0f, 0.0f), 6.0f };
+	Circle c{ Vec2(0.0f, 0.0f), 3.0f };
+	Circle d{ Vec2(0.0f, 0.0f), 4.0f };
+	Circle e{ Vec2(0.0f, 0.0f), 5.0f };
 	v.push_back(a);
 	v.push_back(b);
 	v.push_back(c);
 	v.push_back(d);
 	v.push_back(e);
-	std::sort (v.begin(), v.end(), [](Circle a, Circle b) { return a.radius() < b.radius();});
+	std::sort(v.begin(), v.end(), [](Circle const& a, Circle const& b){ return a.radius() < b.radius(); });
 	REQUIRE(std::is_sorted(v.begin(), v.end()));
 }
+//3.10
+TEST_CASE("describe_tranform", "[aufgabe10]"){
+	std::vector<int> v1{1, 2, 3, 4, 5, 6, 7, 8, 9};
+	std::vector<int> v2{9, 8, 7, 6, 5, 4, 3, 2, 1};
+	std::vector<int> v3(9);
+	std::transform(v1.begin(), v1.end(), v2.begin(), v3.begin(), [](int a, int b){return a+b;});
+	REQUIRE(std::all_of(v3.begin(), v3.end(), [](int a){return a == 10;}));
+}
+//3.11
+TEST_CASE ("describe_filter", "[aufgabe11]"){
+    std::vector<int> v{1, 2, 3, 4, 5, 6};
+    std::vector<int> alleven = filter(v, is_even);
+	REQUIRE(std::all_of(alleven.begin(), alleven.end(), is_even));
+}
 
-int main ( int argc , char * argv []){
-return Catch :: Session (). run ( argc , argv );
+
+int main(int argc, char* argv[]){
+	return Catch::Session().run(argc, argv);
 }
 
--- source/bsp.cpp
+++ source/bsp.cpp
@@ -5,29 +5,39 @@
 #include <iterator>
 #include <algorithm>
 
-
-int main(){
-	std::vector<int>v0(10);
-	for (std::vector<int>::iterator it = v0.begin();
-			it!=v0.end(); ++it){
-		*it=std::rand();
+int main()
+{
+	std::vector <int> v0 (10);						//10 Stellen großer Vektor wird mit (immer wieder dem gleichen
+													//random shit vollgemacht. v0 = vektor
+	for (std::vector<int>::iterator it=v0.begin(); it!=v0.end();++it)	// it = iterator
+	{
+    	*it=std::rand();		// An der Stelle "it" wird eine random Zahl eingefügt.
 	}
-	std::copy(std::begin(v0), std::end(v0),
-						std::ostream_iterator<int>(std::cout, "\n"));
-	std::list<int>l0(v0.size());
-	std::copy(std::begin(v0), std::end(v0), std::begin(l0));
+		// oder
+		// for ( auto & v : v0 ) {
+		// v = std :: rand ();
+		// }
+	std::copy(std::begin(v0), std::end(v0), 		 //kopiert den oben erstellten Vektor
+	  std::ostream_iterator<int>(std::cout, "\n"));	 //und gibt diesen direkt aus.
 
-	std::list<int>l1(std::begin(l0), std::end(l0));
-	std::reverse(std::begin(l1), std::end(l1));
-	std::copy(std::begin(l1), std::end(l1),
-						std::ostream_iterator<int>(std::cout, "\n"));
+	std::list<int>l0(v0.size());					//erstellt leere liste mit der gleichen größe des Vektors. l0 = liste
 
-	l1.sort();
-	std::copy(l1.begin(), l1.end(),
-						std::ostream_iterator<int>(std::cout, "\n"));
+	std::copy(std::begin(v0), std::end(v0), std::begin(l0)); // kopiert Vektor komplett in Liste.
 
-	std::generate(std::begin(v0), std::end(v0), std::rand);
-	std::copy(v0.rbegin(), v0.rend(),
-						std::ostream_iterator<int>(std::cout, "\n"));
+	std::list<int>l1(std::begin(l0), std::end(l0));	// konstruiert neue Liste und kopiert die Werte der 1. l1 = liste
+	std::reverse(std::begin(l1), std::end(l1));		// dreht die komplette neue Liste um
+	std::copy(std::begin(l1), std::end(l1), 		// kopiert die neue Liste komplett und gibt sie aus.
+	  std::ostream_iterator<int>(std::cout, "\n"));
+
+	l1.sort();										//Sortiert die Liste, wobei die kleinste Stelle in die 
+													// in die erste stelle gesetzt wird.
+	std::copy(l1.begin(), l1.end(), 
+	  std::ostream_iterator<int>(std::cout, "\n")); //printet die neue Liste direkt.
+
+	std::generate(std::begin(v0), std::end(v0), std::rand); //belegt v0 mit neuen random zahlen
+
+	std::copy(v0.rbegin(), v0.rend(), 				
+	  std::ostream_iterator<int>(std::cout, "\n"));	//gibt den vektor v0 neu aus, wobei der
+
 	return 0;
 }
